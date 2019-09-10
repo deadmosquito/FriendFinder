d2af3f50bba102a9f2b9fdd3766e33319db69246
Womp Rat Bullseye <stevensorensen13@gmail,com>
2019-09-10 15:08:49 -0700

    update server.js

--- server.js
+++ server.js
@@ -26,8 +26,8 @@ app.use(express.json());
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 // =============================================================================
 // LISTENER

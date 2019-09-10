31e06ecfb75c66903fd76453e4547fecbd149ea1
Womp Rat Bullseye <stevensorensen13@gmail,com>
2019-09-10 13:23:59 -0700

    update server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;

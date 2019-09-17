dd921106f9ff4a1717157d53fbc50331a307d84b
Womp Rat Bullseye <stevensorensen13@gmail,com>
2019-09-16 18:12:44 -0700

    package.json

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 
 var app = express();
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
